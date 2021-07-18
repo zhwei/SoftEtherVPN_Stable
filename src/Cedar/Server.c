a8afc9d5307ae8c9de5a24cd4513dfa6bb17edd7
ZHANG Wei <zhwei.yes@gmail.com>
2021-07-18 14:41:14 +0800

    enable route push for region cn/jp

--- src/Cedar/Server.c
+++ src/Cedar/Server.c
@@ -10914,7 +10914,7 @@ bool SiIsEnterpriseFunctionsRestrictedOnOpenSource(CEDAR *c)
 
 	if (StrCmpi(region, "JP") == 0 || StrCmpi(region, "CN") == 0)
 	{
-		ret = true;
+		ret = false;
 	}
 
 	return ret;
